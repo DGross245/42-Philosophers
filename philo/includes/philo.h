3d7fa445723bf71515ec5702d309b28aeb8a512b
Daniel Gross <dgross@3-F-4.42heilbronn.de>
2022-10-24 11:03:01 +0200

    +philo init and multi mutex

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 09:05:56 by dgross            #+#    #+#             */
-/*   Updated: 2022/10/23 17:36:35 by dgross           ###   ########.fr       */
+/*   Updated: 2022/10/24 10:26:07 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,25 @@
 # define PHILO_H
 
 # include <pthread.h>
+typedef struct s_data
+{
+	int			nbr;
+}t_data;
+
 typedef struct s_philo
 {
-	pthread_t	*thread_arr;
-	pthread_mutex_t mutex;
+	pthread_t		*thread_arr;
+	pthread_mutex_t *mutex_arr;
+	t_data			*philo;
+	int				philo_nbr;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				max_eat;
 }t_philo;
 
+void	*ft_malloc(size_t size);
+int		ft_atoi(const char	*str);
+void	ft_bzero(void *s, size_t n);
+int		ft_isdigit(int c);
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 09:03:58 by dgross            #+#    #+#             */
-/*   Updated: 2022/10/23 17:42:03 by dgross           ###   ########.fr       */
+/*   Updated: 2022/10/24 11:02:15 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,131 +18,115 @@
 // mutex ist einfach ein schutz, damit threads nicht sachen gleichzeitg etwas machen sonden warten bis sie dürfen
 
 // number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
-
-// vllt. zu einer anderen lösung wechseln als atoi um char *philo_nbr in ein int zu machen (vllt. auch drin lassen für zahlen check idk)
-
-// threads gehen nicht in idk_yet rein und führen nicht printf aus!
-void kill_thread(t_philo *philo);
-void create_thread(char *philo_nbr, t_philo *philo);
-void *idk_yet();
-void *ft_malloc(size_t size);
-int	ft_atoi(const char	*str);
-void ft_bzero(void *s, size_t n);
+//[argv1 is die anzahl der philos, mutexes und forks]
+int		destroy(t_philo *philo);
+int		create(t_philo *philo);
+void	*routine(void *data);
+void	*ft_malloc(size_t size);
+int		ft_atoi(const char	*str);
+void	ft_bzero(void *s, size_t n);
+void	init_philo(t_philo	*philo, int argc, char **argv);
+int		error_check(int argc, char **argv);
 
 int	main(int argc, char **argv)
 {
 	t_philo	philo;
 
-	if (argc < 5)
+	if (error_check(argc, argv) == -1)
+	{
+		printf("error\n");
 		return (-1);
-	pthread_mutex_init(&philo.mutex, NULL);
-	create_thread(argv[1], &philo);
-	kill_thread(&philo);
-	pthread_mutex_destroy(&philo.mutex);
+	}
+	init_philo(&philo, argc, argv);
+	create(&philo);
+	destroy(&philo);
 	return (0);
 }
 
-void create_thread(char *philo_nbr, t_philo *philo)
+int error_check(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = 0;
+	if (argc < 5)
+		return (-1);
+	if (argc > 6)
+		return (-1);
+	while (argv[i] != NULL)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (ft_isdigit(argv[i][j]) == 0)
+				return (-1);
+			j++;	
+		}
+		i++;
+	}
+	return (0);
+}
+
+void init_philo(t_philo	*philo, int argc, char **argv)
+{
+	//int i;
+
+	//i = 0;
+	
+	//while (++i < philo->philo_nbr + 1)
+	//	philo->philo[i].nbr = i;
+	philo->philo_nbr = ft_atoi(argv[1]);
+	philo->time_to_die = ft_atoi(argv[2]);
+	philo->time_to_eat = ft_atoi(argv[3]);
+	philo->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		philo->max_eat = ft_atoi(argv[5]);
+	else
+		philo->max_eat = -1;
+}
+
+int create(t_philo *philo)
 {
 	int	i;
 
 	i = -1;
-	philo->thread_arr = ft_malloc(sizeof(pthread_t) * ft_atoi(philo_nbr));
-	while (philo->thread_arr[++i] != NULL)
-	{ 
-		pthread_create(&philo->thread_arr[i], NULL, &idk_yet, NULL);
-	}
+	philo->thread_arr = ft_malloc(sizeof(pthread_t) * (philo->philo_nbr));
+	while (++i < philo->philo_nbr)
+		if (pthread_create(&philo->thread_arr[i], NULL, &routine, NULL) != 0)
+			return (2);
+	i = -1;
+	philo->mutex_arr = ft_malloc(sizeof(pthread_mutex_t) * (philo->philo_nbr));
+	while (++i < philo->philo_nbr)
+		if (pthread_mutex_init(&philo->mutex_arr[i], NULL) != 0)
+			return (3);
+	return (0);
 }
 
-void *idk_yet(void *kek)
+void *routine(void *data)
 {
 	t_philo *philo;
 
-	philo = (t_philo *)kek;
-	printf("thread done\n");
-	return(kek);
+	philo = (t_philo *)data;
+	// take folk 
+	// eat
+	// sleep
+	// think
+	// repeat
+	return(NULL);
 }
 
-void kill_thread(t_philo *philo)
+int destroy(t_philo *philo)
 {
 	int	i;
 
 	i = -1;
 	while (philo->thread_arr[++i] != NULL)
-	{
 		if (pthread_join(philo->thread_arr[i], NULL) != 0)
-			return ;
-	}
-}
-
-#include <stdlib.h> // malloc free
-
-void	*ft_malloc(size_t size)
-{
-	void	*ptr;
-
-	ptr = malloc(size);
-	if (ptr)
-		ft_bzero(ptr, size);
-	else
-	{
-		free(ptr);
-		exit(1);
-	}
-	return (ptr);
-}
-
-static int	isnumber(const char *str, int i, int sign, long int result1)
-{
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		if (result1 * sign > 2147483647)
-			return (-1);
-		if (result1 * sign < -2147483648)
-			return (0);
-		result1 = 10 * result1 + (str[i] - '0');
-		i++;
-	}
-	return (result1);
-}
-
-int	ft_atoi(const char	*str)
-{
-	int			i;
-	int			sign;
-	long int	result;
-
-	i = 0;
-	result = 0;
-	sign = 1;
-	while (str[i] == '\t'
-		|| str[i] == '\n' || str[i] == '\v'
-		|| str[i] == '\f' || str[i] == '\r'
-		|| str[i] == ' ')
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			sign = sign * -1;
-		i++;
-	}
-	result = isnumber(str, i, sign, result) * sign;
-	return (result);
-}
-
-#include <stddef.h> // size_t
-
-void	ft_bzero(void *s, size_t n)
-{
-	unsigned char	*ptr;
-	unsigned char	c;
-
-	c = 0;
-	ptr = s;
-	while (n)
-	{
-		*ptr = c;
-		ptr++;
-		n--;
-	}
+			return (4);
+	i = -1;
+	while (++i < philo->philo_nbr)
+		if (pthread_mutex_destroy(&philo->mutex_arr[i]) != 0)
+			return (5);
+	return (0);
 }
